791f8690e9093e737a73d68ad5802b931bab7d6a
pathfindermilan <pathfinder.milan@protonmail.com>
2025-02-22 14:46:11 +0100

    [UPD] Updated the database to use the postgres engine

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 source /backend/.venv/bin/activate
-exec "$@"
-uv run manage.py runserver 0.0.0.0:8000 &
-
-wait -n
-
-exit $?
+uv run manage.py runserver 0.0.0.0:8000
--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -57,16 +57,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'settings.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
    'default': {
-       'ENGINE': os.getenv('DB_ENGINE'),
+       'ENGINE': 'django.db.backends.postgresql',
        'NAME': os.getenv('DB_NAME'),
        'HOST': os.getenv('DB_HOST'),
        'USER': os.getenv('DB_USER'),
-       'PASSWORD': os.getenv('DB_PASSWORD')
+       'PASSWORD': os.getenv('DB_PASSWORD'),
+       'HOST': os.getenv('DB_HOST', 'localhost'),
+       'PORT': os.getenv('DB_PORT', '5432'),
    }
 }
 
